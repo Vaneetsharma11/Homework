99f816fc11b2e646e4d40f5ef43338e96e1d54e9
Vaneetsharma11 <vaneet11webs@gmail.com>
2019-08-27 18:33:04 -0400

    Initial commit

---
+++ src/quiz/Exam1.java
@@ -0,0 +1,24 @@
+package quiz;
+
+public class Exam1 {
+    private String subject1 = "70";
+    private String subject2 = "60";
+    private String subject3 = "90";
+    private String subject4 = "100";
+
+    public Exam1 (String english, String math, String science, String spanish) {
+
+
+
+
+    public Exam1(String subject1, String subject2, String subject3, String subject4) {
+
+
+            this.subject1 = english;
+            this.subject2 = math;
+            this.subject3 = science;
+            this.subject4 = spanish;
+
+
+        }
+    }
---
+++ src/quiz/main.java
@@ -0,0 +1,14 @@
+package quiz;
+
+public class main {
+
+    public static void main (String[] args) {
+
+        Exam1 exam = new Exam1();
+        System.out.println(subject);
+
+
+
+    }
+
+}
